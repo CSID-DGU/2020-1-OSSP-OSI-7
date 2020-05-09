6fb0d787ea4e24bdd2a3757f8f193e279695d074
yongsk0066 <yongsk0066@naver.com>
2020-05-09 19:44:22 +0900

    :construction:

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,33 +1,49 @@
-import React, {useState} from 'react';
-import {Button} from 'react-bootstrap';
+import React, {useState, useEffect} from 'react';
+import {Button, Container} from 'react-bootstrap';
 import QuizForm from './QuizForm';
 
 
 
 
 const Quiz = ()=>{
-    const [count, setCount] = useState(1);
+    const [count, setCount] = useState(0);
     const [quizzes, setQuizzes] = useState([]);
 
     const addQuiz = (quiz) =>{
-        setQuizzes([...quizzes,""]);
+        setCount(count +1);
+        setQuizzes([...quizzes,quiz]);
     }
 
+    const removeQuiz = (index)=>{
+        quizzes.splice(index,1);
+        console.log(quizzes);
+        setQuizzes([quizzes]);
+        setCount(count-1);
+    }
+
+
+
     return (
-    <>
+    <Container>
         <h1>
             Quiz
         </h1>
-        <h2>{count}</h2>
-        <Button onClick={(e)=>addQuiz(e)}>UP</Button>
+        <h2>TOTAL : {count}</h2>
         {
             quizzes.map((quiz,index)=>{
+                console.log(count);
                 return (
-                    <QuizForm index={index}/>
+                    <QuizForm index={count} type={quiz.type} removeQuiz={removeQuiz}/>
                 )
             })
         }
-    </>
+        <Container>
+        <Button onClick={()=>{addQuiz({type:"mul_choices"})}}>객관식</Button>
+        <Button onClick={()=>{addQuiz({type:"essay"})}}>주관식</Button>
+        <Button onClick={()=>{addQuiz({type:"short_answer"})}}>단답형</Button>
+        <Button onClick={()=>{addQuiz({type:"binary"})}}>OX형</Button>
+        </Container>
+    </Container>
     );
 }
 
--- frontend/src/components/QuizForm.js
+++ frontend/src/components/QuizForm.js
@@ -1,30 +1,56 @@
 import React, {useState} from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, Form, Dropdown, Container, Row, Col, Badge} from 'react-bootstrap';
+
+
+
 
 const QuizForm = (props)=>{
     const [id, setId] = useState(props.index);
-    const [type, setType] = useState("mul_choices");
+    const [type, setType] = useState(props.type);
     const [question, setQuestion] = useState("");
-    const quizType = (type)=>{
-        if(type ==="mul_choices"){
-        return(
-            <h2>hello!!!!!!!!!</h2>
-        );}else {
-            return(<h3>nono</h3>);
-        }
-    }
+    const koreanDict = {"mul_choices":"객관식", "essay":"주관식", "short_answer":"단답형","binary":"OX형"};
+    const removeQuiz = props.removeQuiz;
+    // const convert2Korean = (quizType)=>{
 
+    // }
     return(
-        <>
-        <h1>{type} - no{id}</h1>
-        <input></input>
-        {quizType(type)}
-        <Button onClick={(e)=>{setType("mul_choices")}}>객관식</Button>
-        <Button onClick={()=>{setType("essay")}}>주관식</Button>
-        <Button onClick={()=>{setType("short_answer")}}>단답형</Button>
-        <Button onClick={(e)=>{setType("binary");console.log(e.text)}}>OX형</Button>
+        <Container>
+        <Row>
+            <Col md={"auto"} className="mr-auto">
+            <Button variant="secondary">문항 {id}</Button>
+            </Col>
 
-        </>
+            <Col md={"auto"}>
+            <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                {koreanDict[type]}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item onClick={()=>{setType("mul_choices")}}>객관식</Dropdown.Item>
+                <Dropdown.Item onClick={()=>{setType("essay")}}>주관식</Dropdown.Item>
+                <Dropdown.Item onClick={()=>{setType("short_answer")}}>단답형</Dropdown.Item>
+                <Dropdown.Item onClick={()=>{setType("binary")}}>OX형</Dropdown.Item>
+            </Dropdown.Menu>
+            </Dropdown>
+            </Col>
+            <Col md={"auto"}>
+                <Button variant="danger" onClick={()=>removeQuiz(id-1)}>X</Button>
+            </Col>
+        </Row>
+
+
+        <Form>
+        <Form.Group>
+            <Form.Label>Question</Form.Label>
+            <Form.Control></Form.Control>
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>Description</Form.Label>
+            <Form.Control></Form.Control>
+        </Form.Group>
+        </Form>
+
+        </Container>
         );
 }
 
