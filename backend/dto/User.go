2f9a631ce9cfd8ad66835bc1bf8ea715b7f383a3
4whomtbts <4whomtbts@gmail.com>
2020-05-26 15:48:10 +0900

    Add password field to user dto

--- backend/dto/User.go
+++ backend/dto/User.go
@@ -3,6 +3,7 @@ package dto
 type User struct {
 	UserId int64 `db:"user_id" json:"-"`
 	UserName string `db:"username" json:"username" example:"pigeon1234"`
+	Password string `db:"password" json:"password"`
 	StudentCode int64 `db:"student_code" json:"student_code" example:"2015123456"`
 	Email string `db:"email" json:"email" example:"donggukmail@dgu.ac.kr"`
 	NickName string `db:"nickname" json:"nickname" example:"my nickname"`
