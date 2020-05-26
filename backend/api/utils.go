550537c49b9baad7c43e872f8a72535187743e3d
4whomtbts <4whomtbts@gmail.com>
2020-05-26 19:25:31 +0900

    none

--- backend/api/utils.go
+++ backend/api/utils.go
@@ -6,8 +6,8 @@ func CORSMiddleWare() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
-		c.Header("Access-Control-Allow-Origin", "https://34.64.101.170:8000")
-		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Origin", "*")
+		//c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
 
 		if c.Request.Method == "OPTIONS" {
