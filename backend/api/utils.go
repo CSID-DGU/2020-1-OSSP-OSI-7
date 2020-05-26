9cf8c5bdd0381a713fd221bcc5e102205c85aaee
4whomtbts <4whomtbts@gmail.com>
2020-05-26 19:13:34 +0900

    Abort CORS when OPTIONS

--- backend/api/utils.go
+++ backend/api/utils.go
@@ -4,10 +4,16 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleWare() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
-		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Origin", "https://34.64.101.170:8000")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 
 		c.Next()
 	}
