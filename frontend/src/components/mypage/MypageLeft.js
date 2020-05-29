004dcc521c66fb4068a0d17c915efa58ec77c762
yongsk0066 <yongsk0066@naver.com>
2020-05-29 17:21:05 +0900

    mypage auth show

--- frontend/src/components/mypage/MypageLeft.js
+++ frontend/src/components/mypage/MypageLeft.js
@@ -2,6 +2,7 @@ import React, {Fragment, useState} from 'react';
 import {Image, Row, Col} from 'react-bootstrap';
 import UserClassList from './UserClassList';
 import ToggleSwitch from './ToggleSwitch';
+import ReactTextTransition from 'react-text-transition';
 
 import {classdata} from './classData';
 
@@ -15,18 +16,26 @@ const MypageLeft = (props)=>{
         <Fragment>
         <Image src={avatar} fluid className="profile__img"/>
         <Row>
-        <Col xs={7}>
-        <h3>2015111888</h3>
+            <Col xs={7}>
+            <h3>2015111888</h3>
+            </Col>
+            <Col xs={5} className="toggle__container">
+            <ToggleSwitch
+            isOn={auth}
+            onColor={"#06D6A0"}
+            handleToggle={() => setAuth(!auth)}
+            />
+            </Col>
+        </Row>
+
+        <Row>
+        <Col xs={6}>
+            <h5 className="profile__username">{user}</h5>
         </Col>
-        <Col xs={5} className="toggle__container">
-        <ToggleSwitch
-        isOn={auth}
-        onColor={"#06D6A0"}
-        handleToggle={() => setAuth(!auth)}
-        />
+        <Col xs={6}>
+            <ReactTextTransition text={auth ? "Professor" : "Student"} className="profile__auth" />
         </Col>
         </Row>
-        <h5 className="profile__username">{user}</h5>
         <hr/>
         <UserClassList classes={classes}/>
         </Fragment>
